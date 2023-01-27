ae54e4544664b58e78156609d1c76338614063d4
Jonathan Levine <jonathanaaron.levine@gmail.com>
2023-01-27 16:01:52 -0500

    .....

---
+++ main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import pandas as pd
+from psycopg2 import sql
+from typing import Union
+import numpy as np
+from tqdm import tqdm
+import torch
+
+
+if __name__ == '__main__':
+    print("Hello World!")
